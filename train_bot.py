a43f1990b0da302a05d3bf5d1b605c8d39c1039c
mayan1988 <m.khazaei1988@gmail.com>
2025-09-03 12:43:14 +0330

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TELEGRAM_BOT_TOKEN = "توکن_ربات_تو_اینجا"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! من ربات چک‌کننده قطار هستم.")
+
+def main():
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if name == "main":
+    main()
